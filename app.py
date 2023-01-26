402bb82ad8e341ee58e3585c9f3916922545f8b4
monkeymademe <raspberryjamberlin@gmail.com>
2023-01-26 23:21:01 +0000

    Added loads of comments and also adjusted the code to allow enabling and disabling features without having to delete or comment out mass of code

--- app.py
+++ app.py
@@ -1,26 +1,47 @@
- #!/usr/bin/python
+#!/usr/bin/python
 
 from flask import Flask, request, render_template, jsonify, json
- #, session, copy_current_request_context
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 from flask_socketio import SocketIO, emit
-import time, random, socket
-from sense_hat import SenseHat
-sense = SenseHat()
-from mote import Mote
-mote = Mote()
-mote.configure_channel(1, 16, False)
-mote.configure_channel(2, 16, False)
-mote.configure_channel(3, 16, False)
-mote.configure_channel(4, 16, False)
+import time, random
+
+# Socket (not SocketIO) is used to receive TCP data from the PicoW
+# Set enablePicoData to false if you wish to disable this featue
+enablePicoData = False
+if enablePicoData:
+    import socket
+
+# Feedparser is required for the bbc headline feed.
+# Set enableNews to false to disable the feature
+enableNews = True
+if enableNews:
+    import feedparser
+
+# Sense hat is for the sensor data normally on the pi running the server.
+# Set enableSense to false to disable the featue
+enableSense = True
+if enableSense:
+    from sense_hat import SenseHat
+    sense = SenseHat()
+
+# Mote is for the Pimoroni Mote stick LEDs.
+# Set enableLights to false or replace this code and the code in the lights fuction with your own LED system
+enableLights = True
+if enableLights:
+    from mote import Mote
+    mote = Mote()
+    mote.configure_channel(1, 16, False)
+    mote.configure_channel(2, 16, False)
+    mote.configure_channel(3, 16, False)
+    mote.configure_channel(4, 16, False)
 
 # Create an instance of flask called "app" and setup socketio
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-# Setup apscheduler job pools
+# Setup apscheduler job pool
 executors = {'default': ThreadPoolExecutor(16), 'processpool': ProcessPoolExecutor(4)}
 sched = BackgroundScheduler(timezone='Asia/Seoul', executors=executors)
 
@@ -30,15 +51,19 @@ shipClass = "California"
 shipRegistry = "NCC-75567"
 colorSchemeName = "Ritos Yellow"
 
-# Below is the list of possible color schemes available in the Ritos LCARS. For a little fun uncomment both lines if you want to set the theme randomly when the server boots
-#possibleColorScheme = ["Ritos Yellow", "Sickbay Aqua", "Picard Gray", "Nemesis Cyan", "Padd Blue", "Titan PurpleHead", "Vancouver Orange", "Merced Green", "Oakland Glory", "Inglewood Violet", "Carlsbad Nature", "Prodigy Blast", "Ketracel White", "Disco Beige", "Shipyard Blueprint", "Shipyard Blueprint II", "Red Alert"]
-#colorSchemeName = random.choice(possibleColorScheme)
+# Below is the list of possible color schemes available in the Ritos LCARS.
+# For a little fun set enableRandomColorScheme to true if you want to set the theme randomly when the server boots
+enableRandomColorScheme = False
+if enableRandomColorScheme:
+    possibleColorScheme = ["Ritos Yellow", "Sickbay Aqua", "Picard Gray", "Nemesis Cyan", "Padd Blue", "Titan PurpleHead", "Vancouver Orange", "Merced Green", "Oakland Glory", "Inglewood Violet", "Carlsbad Nature", "Prodigy Blast", "Ketracel White", "Disco Beige", "Shipyard Blueprint", "Shipyard Blueprint II", "Red Alert"]
+    colorSchemeName = random.choice(possibleColorScheme)
 
 # Merge Above defaults into JSON
 serverstatus = {"alertStatus":"normal", "colorSchemeName":colorSchemeName, "shipName":shipName, "shipClass":shipClass, "shipRegistry":shipRegistry, "themeColor":"rgb(255, 211, 0)", "themeColorR":255, "themeColorG":211, "themeColorR":0}
 
 # Pulling Sensor data from sensehat
-sensordata = {"Temperature":round(sense.temp, 2) , "Humidity":round(sense.humidity, 2) , "Pressure":round(sense.pressure, 2)}
+if enableSense:
+    sensordata = {"Temperature":round(sense.temp, 2) , "Humidity":round(sense.humidity, 2) , "Pressure":round(sense.pressure, 2)}
 
 # Had issues with JSON from clients JS so this function cleans up the JSON for Python
 def fixjson(data):
@@ -77,6 +102,7 @@ def setStatus(data):
 def my_form():
     return render_template('index.html')
 
+# Testing post method
 @app.route('/process', methods=['POST'])
 def process():
     text = request.form['testing']
@@ -85,29 +111,30 @@ def process():
 
 # Function for the lights - Using Pimoroni mote sticks
 def lights():
-    global redalertstatus
-    rgb = (0, 0, 0)
-    r, g, b = rgb
-    increment = 1
-    while True:
-        if serverstatus["alertStatus"] == "red":
-            r += increment
-            if r >= 255:
-                r = 255
-                increment = -1
-            if r <= 0:
-                r = 0
-                increment = 1
-            mote.set_all(r, g, b)
-            mote.show()
-            time.sleep(0.00005)
-        if serverstatus["alertStatus"] == "normal":
-            if r >= 1:
-                increment = -1
+    if enableLights:
+        global redalertstatus
+        rgb = (0, 0, 0)
+        r, g, b = rgb
+        increment = 1
+        while True:
+            if serverstatus["alertStatus"] == "red":
                 r += increment
-            mote.set_all(r, g, b)
-            mote.show()
-            time.sleep(0.00005)
+                if r >= 255:
+                    r = 255
+                    increment = -1
+                if r <= 0:
+                    r = 0
+                    increment = 1
+                mote.set_all(r, g, b)
+                mote.show()
+                time.sleep(0.00005)
+            if serverstatus["alertStatus"] == "normal":
+                if r >= 1:
+                    increment = -1
+                    r += increment
+                mote.set_all(r, g, b)
+                mote.show()
+                time.sleep(0.00005)
 
 # Function sending sensor data to all clients 
 def getsensordata():
@@ -116,6 +143,8 @@ def getsensordata():
     socketio.emit('updateSensorData', sensordata, broadcast=True)
 
 # If you have a pico w this little gem receives the data from the pico temp sensor and formats it and pushes it to all clients
+# Set enablePicoData to false if you wish to disable this featue
+enablePicoData = False
 def getpicowudp():
     udp_ip = '0.0.0.0'
     udp_port = 5005
@@ -131,12 +160,23 @@ def getpicowudp():
         print(msg)
         socketio.emit('remoteLogMsg', data, broadcast=True)
 
+def getbbcheadline():
+    feed_url = "http://feeds.bbci.co.uk/news/rss.xml"
+    feed = feedparser.parse(feed_url)
+    entry = feed.entries[0]
+    msg = "Viewing historical news data: " + entry.title
+    data = {"message": msg, "showTimePrefix": True, "playSound": True}
+    print(msg)
+    socketio.emit('remoteLogMsg', data, broadcast=True)
+
 # Adding functions as a scheduled jobs because its a while loop running forever it should run in its own thread
 sched.add_job(lights)
-# Comment out the next line if you don't need the sensehat data
-sched.add_job(getsensordata, 'interval', minutes=2)
-# Comment out the next line if you don't have a Pico W sending temperture data
-sched.add_job(getpicowudp)
+if enableSense:
+    sched.add_job(getsensordata, 'interval', minutes=2)
+if enableNews:
+    sched.add_job(getbbcheadline, 'interval', minutes=10)
+if enablePicoData:
+    sched.add_job(getpicowudp)
 
 # If we're running this script directly, this portion executes. The Flask
 #  instance runs with the given parameters. Note that the "host=0.0.0.0" part
